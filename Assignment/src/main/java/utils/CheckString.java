1efeb938feb849439aad2f6522d44d9967ae3d21
DESKTOP-FQV8T5U\Acer <nguyendhph23086@fpt.edu.vn>
2023-05-09 16:56:53 +0700

    no message

--- Assignment/src/main/java/utils/CheckString.java
+++ Assignment/src/main/java/utils/CheckString.java
@@ -2,7 +2,7 @@ package utils;
 
 public class CheckString {
     public static String checkValues(String value,String ten){
-        if (value.isEmpty()){
+        if (value.trim().isEmpty()){
             return "Phải nhập "+ten;
         }
         return "";
